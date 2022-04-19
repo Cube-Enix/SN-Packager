baf848c4397e6bdd7ae6f949e9aa99f8d266fa26
Kaylerr <itscookiebunny49@gmail.com>
2022-04-19 13:32:18 +0100

    packager inital commit
    unity init

---
+++ SNext Packager Tool/Assets/Scripts/FileInfoGetter.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SFB;
+using System.IO;
+using TMPro;
+
+public class FileInfoGetter : MonoBehaviour
+{
+
+    string HTMLPath, pngPath, mainPath, appName;
+    public bool isUnityTest;
+    public TMP_Text debugger;
+
+    public void Start()
+    {
+        mainPath = Application.dataPath + "/Games";
+        if (!Directory.Exists(mainPath))
+        {
+            AddToDebugger("Wait! We're setting up application for the first time.", "Log");
+            Directory.CreateDirectory(mainPath + "/Game Folders (Not Compressed Yet)/");
+            Directory.CreateDirectory(mainPath + "/SNTs (Compressed SNext Files)/");
+
+        }
+        else
+        {
+            AddToDebugger("Found path! Started App.", "Log");
+        }
+    }
+
+    void AddToDebugger(string debugMessage, string messageType)
+    {
+        debugger.text = debugMessage;
+        if(messageType == "Log")
+        {
+            debugger.color = Color.white;
+        }
+
+        if (messageType == "Warning")
+        {
+            debugger.color = Color.yellow;
+        }
+
+        if (messageType == "Error")
+        {
+            debugger.color = Color.red;
+        }
+    }
+
+    public void GameName(TMP_InputField textField)
+    {
+        appName = textField.text;
+        if(!Directory.Exists(mainPath + "/Game Folders (Not Compressed Yet)/" + appName))
+        {
+            Directory.CreateDirectory(mainPath + "/Game Folders (Not Compressed Yet)/" + appName);
+            AddToDebugger("Created a new directory for this game!", "Log");
+
+        } else
+        {
+            AddToDebugger("This directory exists already. Please delete the existing one or choose a new Directory Name.", "Warning");
+        }
+    }
+
+    public void OpenHTMLFile()
+    {
+        SaveResult(StandaloneFileBrowser.OpenFilePanel("Open File", "", "html", true), "HTML");
+    }
+
+    public void OpenImageFile()
+    {
+        SaveResult(StandaloneFileBrowser.OpenFilePanel("Open File", "", "png", true), "PNG");
+    }
+
+    public void Compress()
+    {
+        string GameDirectory = mainPath + "/Game Folders (Not Compressed Yet)/" + appName;
+        string OutputDirectory = mainPath + "/SNTs (Compressed SNext Files)/" + appName + ".zip";
+
+        AddToDebugger("Compressing into a .SNT!", "Log");
+        SNTPackager.instance.compressDirectory(GameDirectory, OutputDirectory, 9);
+        File.Move(OutputDirectory, mainPath + "/SNTs (Compressed SNext Files)/" + appName + ".snt");
+        AddToDebugger("Done!", "Warn");
+
+    }
+
+    public void SaveResult(string[] paths, string methodType)
+    {
+        if(methodType == "HTML")
+        {
+            if (paths.Length == 0)
+            {
+                return;
+            }
+
+            //only uses the first result of the path.
+            HTMLPath = paths[0];
+            File.Copy(HTMLPath, mainPath + "/Game Folders (Not Compressed Yet)/" + appName + "/" + appName + ".html");
+            AddToDebugger("Added .html game file to the game's folder!", "Log");
+
+        } else if(methodType == "PNG")
+        {
+            if (paths.Length == 0)
+            {
+                return;
+            }
+
+            //only uses the first result of the path.
+            pngPath = paths[0];
+            File.Copy(pngPath, mainPath + "/Game Folders (Not Compressed Yet)/" + appName + "/" + appName + ".png");
+            AddToDebugger("Added .png cover image to the game's folder!", "Log");
+
+        } else
+        {
+            AddToDebugger("An unknown error occured.", "Error");
+        }
+    } 
+
+}
---
+++ SNext Packager Tool/Assets/Scripts/SNTPackager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using ICSharpCode.SharpZipLib.Zip;
+
+public class SNTPackager : MonoBehaviour
+{
+    public static SNTPackager instance;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    public void compressDirectory(string DirectoryPath, string OutputFilePath, int CompressionLevel = 9)
+    {
+        // Depending on the directory this could be very large and would require more attention
+        // in a commercial package.
+        string[] filenames = Directory.GetFiles(DirectoryPath);
+
+        // 'using' statements guarantee the stream is closed properly which is a big source
+        // of problems otherwise.  Its exception safe as well which is great.
+        using (ZipOutputStream OutputStream = new ZipOutputStream(File.Create(OutputFilePath)))
+        {
+
+            // Define the compression level
+            // 0 - store only to 9 - means best compression
+            OutputStream.SetLevel(CompressionLevel);
+
+            byte[] buffer = new byte[4096];
+
+            foreach (string file in filenames)
+            {
+                if (!file.Contains(".meta"))
+                {
+
+                    // Using GetFileName makes the result compatible with XP
+                    // as the resulting path is not absolute.
+                    ZipEntry entry = new ZipEntry(Path.GetFileName(file));
+
+                    // Setup the entry data as required.
+
+                    // Crc and size are handled by the library for seakable streams
+                    // so no need to do them here.
+
+                    // Could also use the last write time or similar for the file.
+                    entry.DateTime = System.DateTime.Now;
+                    OutputStream.PutNextEntry(entry);
+
+                    using (FileStream fs = File.OpenRead(file))
+                    {
+
+                        // Using a fixed size buffer here makes no noticeable difference for output
+                        // but keeps a lid on memory usage.
+                        int sourceBytes;
+
+                        do
+                        {
+                            sourceBytes = fs.Read(buffer, 0, buffer.Length);
+                            OutputStream.Write(buffer, 0, sourceBytes);
+                        } while (sourceBytes > 0);
+                    }
+                }
+            }
+
+            // Finish/Close arent needed strictly as the using statement does this automatically
+
+            // Finish is important to ensure trailing information for a Zip file is appended.  Without this
+            // the created file would be invalid.
+            OutputStream.Finish();
+
+            // Close is important to wrap things up and unlock the file.
+            OutputStream.Close();
+
+            Debug.Log("Files successfully compressed");
+        }
+    }
+}
